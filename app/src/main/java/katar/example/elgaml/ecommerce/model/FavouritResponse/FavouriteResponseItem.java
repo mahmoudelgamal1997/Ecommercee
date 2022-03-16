4c8404bbbb801b790fe901885845bd6420bc4811
mahmoudelgamal1997 <mahmoudelgamal1997@gmail.com>
2022-03-16 19:28:26 +0200

    change package name

---
+++ app/src/main/java/katar/example/elgaml/ecommerce/model/FavouritResponse/FavouriteResponseItem.java
@@ -0,0 +1,159 @@
+package katar.example.elgaml.ecommerce.model.FavouritResponse;
+
+public class FavouriteResponseItem{
+	private int bestSeller;
+	private int isOutofstockAllowed;
+	private String ingredientsAr;
+	private String nameAr;
+	private String longDescriptionEn;
+	private int totalRate;
+	private String createdAt;
+	private String ingredientsEn;
+	private String longDescriptionAr;
+	private String updatedAt;
+	private int categoryId;
+	private int subCategoryId;
+	private Price price;
+	private int hotDeals;
+	private int id;
+	private int stock;
+	private String descriptionEn;
+	private boolean isFav;
+	private String url;
+	private int brandId;
+	private boolean isReviewed;
+	private int disable;
+	private int countPaid;
+	private Object providerId;
+	private String defaultImage;
+	private String shippingSpecification;
+	private int activation;
+	private String nameEn;
+	private String descriptionAr;
+	private int status;
+	private String shippingSpecificationsAr;
+
+	public int getBestSeller(){
+		return bestSeller;
+	}
+
+	public int getIsOutofstockAllowed(){
+		return isOutofstockAllowed;
+	}
+
+	public String getIngredientsAr(){
+		return ingredientsAr;
+	}
+
+	public String getNameAr(){
+		return nameAr;
+	}
+
+	public String getLongDescriptionEn(){
+		return longDescriptionEn;
+	}
+
+	public int getTotalRate(){
+		return totalRate;
+	}
+
+	public String getCreatedAt(){
+		return createdAt;
+	}
+
+	public String getIngredientsEn(){
+		return ingredientsEn;
+	}
+
+	public String getLongDescriptionAr(){
+		return longDescriptionAr;
+	}
+
+	public String getUpdatedAt(){
+		return updatedAt;
+	}
+
+	public int getCategoryId(){
+		return categoryId;
+	}
+
+	public int getSubCategoryId(){
+		return subCategoryId;
+	}
+
+	public Price getPrice(){
+		return price;
+	}
+
+	public int getHotDeals(){
+		return hotDeals;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	public int getStock(){
+		return stock;
+	}
+
+	public String getDescriptionEn(){
+		return descriptionEn;
+	}
+
+	public boolean isIsFav(){
+		return isFav;
+	}
+
+	public String getUrl(){
+		return url;
+	}
+
+	public int getBrandId(){
+		return brandId;
+	}
+
+	public boolean isIsReviewed(){
+		return isReviewed;
+	}
+
+	public int getDisable(){
+		return disable;
+	}
+
+	public int getCountPaid(){
+		return countPaid;
+	}
+
+	public Object getProviderId(){
+		return providerId;
+	}
+
+	public String getDefaultImage(){
+		return defaultImage;
+	}
+
+	public String getShippingSpecification(){
+		return shippingSpecification;
+	}
+
+	public int getActivation(){
+		return activation;
+	}
+
+	public String getNameEn(){
+		return nameEn;
+	}
+
+	public String getDescriptionAr(){
+		return descriptionAr;
+	}
+
+	public int getStatus(){
+		return status;
+	}
+
+	public String getShippingSpecificationsAr(){
+		return shippingSpecificationsAr;
+	}
+}
